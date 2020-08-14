e9cc190daf51b47e1902eca63df83b5ee2e12ccc
harisson.nas@gmail.com <ufgh3538>
2020-08-14 05:37:11 -0300

    'Spring Boot 2.3 Essentials - DevDojo Course'

---
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -0,0 +1,47 @@
+package academy.devdojo.springboot2.controller;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("animes")
+@Slf4j
+@RequiredArgsConstructor
+public class AnimeController {
+    private final AnimeService animeService;
+
+    @GetMapping(path = "/list")
+    public ResponseEntity<List<Anime>> listAll() {
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable int id){
+        return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return ResponseEntity.ok(animeService.save(anime));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id){
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@RequestBody Anime anime){
+        animeService.update(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/main/java/academy/devdojo/springboot2/util/Utils.java
@@ -0,0 +1,24 @@
+package academy.devdojo.springboot2.util;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+@Component
+public class Utils {
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("yyyy_MM-dd HH:mm:ss").format(localDateTime);
+    }
+
+    public Anime findAnimeOrThrowNotFound(int id, AnimeRepository animeRepository) {
+        return animeRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
